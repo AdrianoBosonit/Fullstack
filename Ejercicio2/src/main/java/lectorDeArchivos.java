75eafc739af8d6eb7605d022611b032b5f088407
Adriano Bravo Guzmán <adriano.bravo@bosonit.com>
2022-02-10 12:13:25 +0100

    Ejercicio 2

---
+++ Ejercicio2/src/main/java/lectorDeArchivos.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
+
+
+public class lectorDeArchivos {
+    List<Persona> personas;
+    int linea;
+    void run() {
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+        Optional<String> nombre;
+        Optional<String> poblacion;
+        Optional<Integer> edad;
+        personas=new ArrayList();
+
+
+        try {
+            archivo = new File("personas.txt");
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+
+            String linea;
+            String[] linea2;
+            while ((linea = br.readLine()) != null){
+                linea2 = linea.split("\\:");
+                nombre=Optional.of(linea2[0]);
+                poblacion=Optional.empty();
+                edad=Optional.empty();
+                if(linea2.length==2){
+                    if(linea2[1].chars().allMatch((Character::isDigit))){
+                        edad=Optional.of(Integer.parseInt(linea2[1]));
+
+                    }else{
+                        poblacion=Optional.of(linea2[1]);
+                    }
+                }
+                if(linea2.length==3){
+                    if(linea2[1]==""){
+                        edad=Optional.of(Integer.parseInt(linea2[2]));
+                    }else{
+                        poblacion=Optional.of(linea2[1]);
+                        edad=Optional.of(Integer.parseInt(linea2[2]));
+                    }
+                }
+
+
+                personas.add(new Persona(nombre.orElse("desconocido"),poblacion.orElse("desconocido"),edad.orElse(-1)));
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+
+    void representarFichero(){
+        linea=0;
+        personas.stream().filter(persona -> persona.getEdad()<25).forEach(p ->{ System.out.println("Linea:"+linea+": "+p.toString());linea++;});
+    }
+}
---
+++ Ejercicio2/src/main/java/main.java
@@ -0,0 +1,9 @@
+
+public class main {
+    public static void main(String[] args){
+        lectorDeArchivos lector=new lectorDeArchivos();
+        lector.run();
+        lector.representarFichero();
+    }
+
+}
