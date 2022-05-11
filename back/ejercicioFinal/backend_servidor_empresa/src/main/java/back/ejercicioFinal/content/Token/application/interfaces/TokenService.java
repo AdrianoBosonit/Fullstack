b40b621decea9bf0c5aca5012d9802fc352a99d4
Adriano Bravo Guzmán <adriano.bravo@bosonit.com>
2022-05-11 13:12:31 +0200

    Actualiza repo

--- back/ejercicioFinal/backend_servidor_empresa/src/main/java/back/ejercicioFinal/content/Token/application/interfaces/TokenService.java
+++
@@ -1,7 +0,0 @@
-package back.ejercicioFinal.content.Token.application.interfaces;
-
-import back.ejercicioFinal.content.Token.domain.TokenEntity;
-
-public interface TokenService {
-    public TokenEntity add(String header, String token, String username, String password);
-}
--- back/ejercicioFinal/backend_servidor_empresa/src/main/java/back/ejercicioFinal/shared/mailSender/MailSender.java
+++
@@ -1,8 +0,0 @@
-package back.ejercicioFinal.shared.mailSender;
-
-import back.ejercicioFinal.content.Correo.domain.CorreoEntity;
-import back.ejercicioFinal.content.Correo.infrastructure.dto.CorreoInputDto;
-
-public interface MailSender {
-    public CorreoEntity sendEmail(CorreoInputDto correoInputDto);
-}
